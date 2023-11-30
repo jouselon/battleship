144cbfcbe1958583ff8307640a20a54bff60d5a7
heetaelee <josepheelee@gmail.com>
2023-11-30 12:14:31 +0900

    배틀쉽 게임보드

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,93 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    int gameBoardLength = 4;
+    int gameBoardLength = 7;
     char water = '-';
-    char ship = 's';
+    char ship = 'ㅁ';
     char hit = 'X';
     char miss = 'O';
     int shipNumber = 3;
 
     char[][] gameBoard = createGameBoard(gameBoardLength, water, ship, shipNumber);
-    printGameBoard(gameBoard, water,ship);
+    printGameBoard(gameBoard, water, ship);
+    int undetectedShipNumber = shipNumber;
+    while (undetectedShipNumber > 0) {
+      int[] guessCoordinates = getUserCoordinates(gameBoardLength);
+      char locationViewUpdate = evaluateGuessAndGetTheTarget(guessCoordinates, gameBoard, ship, water,  hit, miss);
+      if (locationViewUpdate == hit) {
+        undetectedShipNumber--;
+      }
+      gameBoard = updateGameBaord(gameBoard, guessCoordinates, locationViewUpdate);
+      printGameBoard(gameBoard, water, ship);
+
+    }
 
   }
 
+  private static char[][] updateGameBaord(char[][] gameBoard, int[] guessCoordinates, char locationViewUpdate) {
+    int row = guessCoordinates[0];
+    int col = guessCoordinates[1];
+    gameBoard[row][col] = locationViewUpdate;
+    return gameBoard;
+  }
+
+  private static char evaluateGuessAndGetTheTarget(int[] guessCoordinates, char[][] gameBoard, char ship, char water, char hit, char miss) {
+    String message;
+    int row = guessCoordinates[0];
+    int col = guessCoordinates[1];
+    char target = gameBoard[row][col];
+    if(target == ship){
+      message = "맞췄습니다!";
+      target = hit;
+    }else if (target == water){
+      message = "빗나갔습니다";
+      target = miss;
+
+    }else {
+      message = "이미 맞춘 곳입니다";
+    }
+    System.out.println(message);
+    return target;
+  }
+
+  private static int[] getUserCoordinates(int gameBoardLength) {
+    int row;
+    int col;
+    do {
+      System.out.print("Row: ");
+      row = new Scanner(System.in).nextInt();
+    } while (row < 0 || row > gameBoardLength + 1);
+
+    do{
+    System.out.print("Col: ");
+    col = new Scanner(System.in).nextInt();
+  }while(col<0||col >gameBoardLength +1);
+  return new int[]{row - 1, col - 1};
+}
+
   private static void printGameBoard(char[][] gameBoard, char water, char ship) {
     int gameBoardLength = gameBoard.length;
-    for (int row=0; row<gameBoardLength; row++){
-      for (int col=0; col<gameBoardLength; col++){
-
-      }
+    System.out.print("  ");
+    for (int i = 0; i < gameBoardLength; i++){
+      System.out.print(i + 1 + " ");
     }
+    System.out.println();
+    for (int row=0; row<gameBoardLength; row++){
+      System.out.print(row + 1 + " ");
+      for (int col=0; col<gameBoardLength; col++){
+        char position = gameBoard[row][col];
+        if(position == ship){
+          System.out.print(water + " ");
+        } else {
+          System.out.print(position + " ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.println();
   }
 
   private static char[][] createGameBoard(int gameBoardLength, char water, char ship, int shipNumber) {
@@ -50,7 +116,7 @@ public class Main {
   private static int[] generateShipCoordinates(int gameBoardLength) {
     int[] coordinates = new int[2];
     for (int i = 0; i<coordinates.length; i++){
-      coordinates[i] = new Random().nextInt(gameBoardLength)
+      coordinates[i] = new Random().nextInt(gameBoardLength);
     }
     return coordinates;
   }
